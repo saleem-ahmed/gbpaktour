1eff1218bae372ef879c347068e5382a7f107f12
saleem-ahmed <maranamahmed1234@gmail.com>
2024-01-27 22:55:35 +0500

    some changes

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Header from "./header";
 import Image from "next/image";
 import HedaerImg from "@/public/bgPak.png";
@@ -12,8 +14,15 @@ import Img6 from "@/public/adventure6.jpg";
 import SerImg1 from "@/public/car.png";
 import SerImg2 from "@/public/house.png";
 import SerImg3 from "@/public/food.png";
+import { Footer } from "flowbite-react";
 import { Button } from "flowbite-react";
-
+import {
+  BsFacebook,
+  BsInstagram,
+  BsYoutube,
+  BsWhatsapp,
+  BsMailbox
+} from "react-icons/bs";
 const Home = () => {
   const AdventureCards = [
     {
@@ -117,7 +126,7 @@ const Home = () => {
               We operate outdoor trips to the corners of Gilgit Baltistan across
               old Silk route, Himalaya and karakoram.
             </p>
-            <Button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <Button className="text-white bg-[#0e7490] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-[#0e7490] dark:focus:ring-blue-800">
               Contact Us
             </Button>
           </div>
@@ -126,16 +135,16 @@ const Home = () => {
 
       <div className="bg-white dark:bg-gray-900">
         <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-          <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+          <div className="font-light  sm:text-lg dark:text-gray-400">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-[#0e7490] dark:text-white">
               About us
             </h2>
-            <p className="mb-4">
+            <p className="mb-4 text-gray-500">
               GB Pakistan Travel & Tours is one of the leading Tour Operators in
               Gilgit Baltistan , Pakistan, providing comprehensive services for
               groups and individuals.
             </p>
-            <p>
+            <p className="text-gray-500">
               GB Pakistan Travel & Tours was first established in the year 2010
               by qualified professionals with more than Ten years of experience
               in the tourism industry. Holding places, people and customs
@@ -166,8 +175,8 @@ const Home = () => {
 
       <div className="bg-white dark:bg-gray-900">
         <div className="gap-16 flex flex-col items-center py-8 px-4 mx-auto max-w-screen-xl">
-          <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+          <div className="font-light  sm:text-lg dark:text-gray-400">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-[#0e7490] dark:text-white">
               Adventure Tours
             </h2>
           </div>
@@ -184,8 +193,8 @@ const Home = () => {
                     alt=""
                   />
                   <div className="flex flex-col gap-1 px-3 py-3">
-                    <h3 className="font-bold text-blue-700">{items.title}</h3>
-                    <p className="text-blue-700">{items.desc}</p>
+                    <h3 className="font-bold text-[#0e7490]">{items.title}</h3>
+                    <p className="text-[#0e7490]">{items.desc}</p>
                   </div>
                 </div>
               </>
@@ -197,7 +206,7 @@ const Home = () => {
       <div className="bg-white dark:bg-gray-900">
         <div className="gap-16 flex flex-col items-center py-8 px-4 mx-auto max-w-screen-xl">
           <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-[#0e7490] dark:text-white">
               Cultural Tours
             </h2>
           </div>
@@ -214,8 +223,8 @@ const Home = () => {
                     alt=""
                   />
                   <div className="flex flex-col gap-1 px-3 py-3">
-                    <h3 className="font-bold text-blue-700">{items.title}</h3>
-                    <p className="text-blue-700">{items.desc}</p>
+                    <h3 className="font-bold text-[#0e7490]">{items.title}</h3>
+                    <p className="text-[#0e7490]">{items.desc}</p>
                   </div>
                 </div>
               </>
@@ -226,7 +235,7 @@ const Home = () => {
       <section class="bg-white dark:bg-gray-900 antialiased">
         <div class="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
           <div class="max-w-2xl mx-auto text-center">
-            <h2 class="text-3xl font-extrabold leading-tight tracking-tight text-gray-900 sm:text-4xl dark:text-white">
+            <h2 class="text-3xl font-extrabold leading-tight tracking-tight text-[#0e7490] sm:text-4xl dark:text-white">
               Our Services
             </h2>
             <p class="mt-4 text-base font-normal text-gray-500 sm:text-xl dark:text-gray-400">
@@ -240,7 +249,7 @@ const Home = () => {
                 <div class="space-y-4 flex flex-col items-center">
                   <Image width={52} height={52} src={service.icon} alt="" />
 
-                  <h3 class="text-2xl font-bold leading-tight text-gray-900 hover:text-blue-700 dark:text-white">
+                  <h3 class="text-2xl font-bold leading-tight text-[#0e7490] hover:text-[#0e7490] dark:text-white">
                     {service.name}
                   </h3>
                   <p class="text-lg font-normal text-gray-500 hover:text-blue-500 dark:text-gray-400">
@@ -256,7 +265,7 @@ const Home = () => {
       <section class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
           <div class="mx-auto mb-8 max-w-screen-sm lg:mb-16">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-[#0e7490] dark:text-white">
               Our Amazing Team
             </h2>
             <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
@@ -275,7 +284,7 @@ const Home = () => {
                     src={member.img}
                     alt="Bonnie Avatar"
                   />
-                  <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <h3 class="mb-1 text-2xl font-bold tracking-tight text-[#0e7490] dark:text-white">
                     <a href="#">{member.name}</a>
                   </h3>
                   <p>{member.design}</p>
@@ -283,7 +292,7 @@ const Home = () => {
                     <li>
                       <a
                         href="#"
-                        class="text-[#39569c] hover:text-gray-900 dark:hover:text-white"
+                        class="text-[#39569c] hover:text-[#0e7490] dark:hover:text-white"
                       >
                         <svg
                           class="w-6 h-6"
@@ -302,7 +311,7 @@ const Home = () => {
                     <li>
                       <a
                         href="#"
-                        class="text-[#00acee] hover:text-gray-900 dark:hover:text-white"
+                        class="text-[#00acee] hover:text-[#0e7490] dark:hover:text-white"
                       >
                         <svg
                           class="w-6 h-6"
@@ -317,7 +326,7 @@ const Home = () => {
                     <li>
                       <a
                         href="#"
-                        class="text-gray-900 hover:text-gray-900 dark:hover:text-white dark:text-gray-300"
+                        class="text-[#0e7490] hover:text-[#0e7490] dark:hover:text-white dark:text-gray-300"
                       >
                         <svg
                           class="w-6 h-6"
@@ -336,7 +345,7 @@ const Home = () => {
                     <li>
                       <a
                         href="#"
-                        class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white"
+                        class="text-[#ea4c89] hover:text-[#0e7490] dark:hover:text-white"
                       >
                         <svg
                           class="w-6 h-6"
@@ -362,7 +371,7 @@ const Home = () => {
 
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-[#0e7490] dark:text-white">
             Contact Us
           </h2>
           <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
@@ -373,14 +382,14 @@ const Home = () => {
             <div>
               <label
                 for="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                className="block mb-2 text-sm font-medium text-[#0e7490] dark:text-gray-300"
               >
                 Your email
               </label>
               <input
                 type="email"
                 id="email"
-                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-[#0e7490] text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="name@xyz.com"
                 required
               />
@@ -388,14 +397,14 @@ const Home = () => {
             <div>
               <label
                 for="subject"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                className="block mb-2 text-sm font-medium text-[#0e7490] dark:text-gray-300"
               >
                 Subject
               </label>
               <input
                 type="text"
                 id="subject"
-                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                className="block p-3 w-full text-sm text-[#0e7490] bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Let us know how we can help you"
                 required
               />
@@ -403,20 +412,20 @@ const Home = () => {
             <div className="sm:col-span-2">
               <label
                 for="message"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                className="block mb-2 text-sm font-medium text-[#0e7490] dark:text-gray-400"
               >
                 Your message
               </label>
               <textarea
                 id="message"
                 rows="6"
-                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                className="block p-2.5 w-full text-sm text-[#0e7490] bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Leave a comment..."
               ></textarea>
             </div>
             <button
               type="submit"
-              className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-[#0e7490] sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-[#0e7490]"
             >
               Send message
             </button>
@@ -429,7 +438,7 @@ const Home = () => {
             id="gmap_canvas"
             src="https://maps.google.com/maps?q=Gb%20Pakistan%20tours,%20Gilgit&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed"
             frameborder="0"
-            scrolling="no" 
+            scrolling="no"
             marginheight="0"
             marginwidth="0"
           ></iframe>
@@ -440,7 +449,7 @@ const Home = () => {
         <div className="mx-auto max-w-screen-xl text-center">
           <a
             href="#"
-            className="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white"
+            className="flex justify-center items-center text-2xl font-semibold text-[#0e7490] dark:text-white"
           >
             GB PAK TOURS
           </a>
@@ -448,7 +457,8 @@ const Home = () => {
             We operate outdoor trips to the corners of Gilgit Baltistan across
             old Silk route, Himalaya and karakoram.
           </p>
-          <ul className="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
+
+          <ul className="flex flex-wrap justify-center items-center mb-6 text-[#0e7490] dark:text-white">
             <li>
               <a href="#" className="mr-4 hover:underline md:mr-6 ">
                 Home
@@ -490,6 +500,42 @@ const Home = () => {
           </span>
         </div>
       </footer>
+      <div className="w-full bg-[#0e7490] px-4 py-6 sm:flex sm:items-center sm:justify-center">
+        {/* <Footer.Copyright href="#" by="Flowbite™" year={2022} /> */}
+        <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+          <Footer.Icon
+            className="text-white"
+            target="_blank"
+            href="https://www.facebook.com/profile.php?id=100064152220501"
+            icon={BsFacebook}
+          />
+          <Footer.Icon
+            className="text-white"
+            href="https://www.instagram.com/gbpakistantours/"
+            target="_blank"
+            icon={BsInstagram}
+          />
+          <Footer.Icon
+            className="text-white"
+            href="https://www.youtube.com/channel/UCpfyXPaalaMpAUkEsCcg_Bw"
+            target="_blank"
+            icon={BsYoutube}
+          />
+          <Footer.Icon
+            className="text-white"
+            href="tel:+923319807200"
+            icon={BsWhatsapp}
+          />
+
+          <Footer.Icon
+            className="text-white"
+            href="mailto:gbpakistantours@gmail.com"
+            icon={BsMailbox}
+          />
+          
+              
+        </div>
+      </div>
     </>
   );
 };
